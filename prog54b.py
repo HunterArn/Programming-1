184034d109787a46f9a340f7960479c147c382ef
Hunter Arn <112713265+HunterArn@users.noreply.github.com>
2022-09-12 20:38:59 +0000

    update

---
+++ prog54b.py
@@ -0,0 +1,9 @@
+num1 = float(input("please enter num1 "))
+num2 = float(input("please enter num2 "))
+num3 = float(input("please enter num3 "))
+num4 = float(input("please enter num4 "))
+summ = num1 + num2 + num3 + num4
+average = summ/4
+
+print("summ =", summ)
+print("average =", average)
