f6025fa5cf643f31b98b2f2e6b61aa5bc4b32b72
Hunter Arn <112713265+HunterArn@users.noreply.github.com>
2022-09-20 14:17:17 +0000

    update

---
+++ k316.py
@@ -0,0 +1,104 @@
+from stanfordkarel import*
+
+
+class ktools:
+  def m(self):
+    """Shorthand for move"""
+    move()
+
+  def tl(self):
+    """Turn Left"""
+    turn_left()
+
+  def tr(self):
+    """Turn Right"""
+    self.tl()
+    self.tl()
+    self.tl()
+
+  def ta(self):
+    """Turn Around"""
+    self.tl()
+    self.tl()
+
+  def pick(self):
+    """pick_beeper"""
+    pick_beeper()
+
+  def put(self):
+   """put_beeper"""
+   put_beeper()
+
+  def put2(self):
+     """put 2 beepers in a line"""
+     self.put()
+     self.m()
+     self.put()
+     
+  def put5(self):
+     """put 5 beepers in a line"""
+     self.put2()
+     self.m()
+     self.put2()
+     self.m()
+     self.put()
+
+  def m2(self):
+     self.m()
+     self.m()
+
+
+  def m3(self):
+     self.m2()
+     self.m()
+
+  def m4(self):
+     self.m2()
+     self.m2()
+  
+  def pick2(self):
+     self.pick()
+     self.m()
+     self.pick()
+
+  def pick3(self):
+     self.pick()
+     self.m()
+     self.pick()
+     self.m()
+     self.pick()
+  
+  
+  def pick4(self):
+     self.pick()
+     self.m()
+     self.pick()
+     self.m()
+     self.pick()
+     self.m()
+     self.pick()
+
+  
+def main():
+    """ Karel code goes here! """
+    kt = ktools()
+ 
+    kt.m2()
+    kt.tl()
+    kt.m2()
+    kt.pick4()
+    kt.tr()
+    kt.m()
+    kt.pick3()
+    kt.tr()
+    kt.m()
+    kt.pick3()
+    kt.tr()
+    kt.m()
+    kt.pick2()
+  
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
